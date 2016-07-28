fd363d7645380061d4bbbc8a78d5e4319fd693c7
Ryan Gerlach <gerlachry@gmail.com>
2016-07-27 20:05:45 -0400

    ensure latest react

--- juno/static/layout.js
+++ juno/static/layout.js
@@ -2,9 +2,9 @@
     import React from 'react';
     import ReactDOM from 'react-dom';
     import { FilterChart as Component} from 'pyxley';
-    var charts = [{"options": {"url": "/sensors-chart/", "chart_id": "sensors-chart", "params": {"y_accessor": "y", "x_accessor": "x", "animate_on_load": "true", "target": "#sensors-chart", "init_params": {"Data": "Basement Temperature"}, "bottom": 30, "width": 1200, "transition_on_update": "true", "left": 40, "buffer": 8, "small_width_threshold": 160, "small_height_threshold": 120, "description": "Data from IoT sensors", "top": 40, "height": 300, "right": 40, "title": "Sensor Data"}}, "type": "MetricsGraphics"}];
+    var charts = [{"type": "MetricsGraphics", "options": {"url": "/sensors-chart/", "chart_id": "sensors-chart", "params": {"small_height_threshold": 120, "animate_on_load": "true", "top": 40, "description": "Data from IoT sensors", "width": 1200, "height": 300, "small_width_threshold": 160, "target": "#sensors-chart", "y_accessor": "y", "title": "Sensor Data", "transition_on_update": "true", "x_accessor": "x", "bottom": 30, "init_params": {"Data": "Basement Temperature"}, "left": 40, "buffer": 8, "right": 40}}}];
 var filter_style = "''";
-var filters = [{"options": {"label": "Data", "alias": "Data", "default": "Basement Temperature", "items": ["Basement Temperature", "Basement Humidity"]}, "type": "SelectButton"}];
+var filters = [{"type": "SelectButton", "options": {"alias": "Data", "default": "Basement Temperature", "items": ["Basement Temperature", "Basement Humidity"], "label": "Data"}}];
 var dynamic = true;
     ReactDOM.render(
         <Component
